3c241948c059ce82a68afc62a98e97c52a26253e
Sanjeev Kumar <sanjaykumar2k14.sk@gmail.com>
2026-03-13 16:23:29 +0530

    Add files via upload

---
+++ tshomework/switch.js
@@ -0,0 +1,28 @@
+let day = 4
+switch(day){
+    case 1:
+    console.log("day is monday")
+    break
+
+    case 2:
+        console.log("today is tuesday")
+        break
+
+    case 3:
+        console.log("Today is wednesday")
+        break
+        
+    case 4:
+        console.log("Today is thrusday")
+        break
+     
+    default:
+        console.log("Today is holiday")
+        break    
+}
+
+//reporter: [['html',{open:always}]],
+ //trace: 'on',
+   // headless:false,
+    //video:'on',
+    //screenshot:'on',
